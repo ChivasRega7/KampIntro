95cd3c5e63f9797467f6f962a27bc1672c7238f2
HAMDI EKMEKCI <hamdiekmekcii34@gmail.com>
2023-07-27 17:10:05 +0300

    .NET

---
+++ OOP1/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{   
+    //snippet
+    class Product //Entity
+    {
+        public int Id { get; set; }
+        
+        public int CategoryId { get; set; }
+        //Ürün Adı
+        public string ProductName { get; set; }
+        //Adet Fiyatı
+        public double UnitPrice { get; set; }
+        //Stok Adedi
+        public int UnitsInStock { get; set; }
+
+     // INFO --> CRUD (Create,Read,Update,Delete)
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa"; //Gerçekte Kullanıcı verisi almak için => product1.ProductName = tbxProductName.Text
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+            
+           //PascalCase    //camelCase
+           //case sensitive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine("Id : " + product1.Id + " \nCategory Id : " + product1.CategoryId + " \nProduct Name : " + product1.ProductName + "\nPrice : " + product1.UnitPrice + "\nStock : " + product1.UnitsInStock);
+
+            int sayi = 100;
+            productManager.BiseyYap(sayi);
+            Console.WriteLine(sayi);
+            //=100 ->It doesn't change because we are giving 
+
+
+            //int,double,bool ... değer tip
+            //arrays , class , abstract calass , Interface... referans tip
+        }
+    }
+}
